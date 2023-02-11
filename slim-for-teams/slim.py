3f4ab10c1f49f7d7cab52ded9f5eda2745156385
Annika Stein <annika-stein@t-online.de>
2023-02-11 10:58:52 +0100

    column selection for respondent group and team or committee

--- slim-for-teams/slim.py
+++ slim-for-teams/slim.py
@@ -1,12 +1,21 @@
 import numpy as np
 import csv
 import pandas as pd
+import argparse
+import scheme
 
-survey_group = 'VS' # 'Staff'
+parser = argparse.ArgumentParser(description="Setup survey groups and team")
+parser.add_argument("--group", choices=['VS', 'VC1', 'VC2'], help="Group of respondents")
+parser.add_argument("--team", choices=['WCT', 'WFC', 'WRC', 'WQAC', 'WDC', 'WST'], help="WCA Team or Committee")
+args = parser.parse_args()
+
+survey_group = args.group
+
+team_committee = args.team
+
+#survey_group = 'VS' # 'Staff'
 #survey_group = 'VC1' # 'Community-1_withID'
 #survey_group = 'VC2' # 'Community-2_noID'
-
-main_path = 'WCA_Survey_2022/'
 
 def survey_loader(fpath):
     # loads the survey as specified via the file path into a dataframe
@@ -14,52 +23,22 @@ def survey_loader(fpath):
     print(response)
     return response
 
-def get_question_answer_scheme(dataframe):
-    return dataframe[:1]
-
-validated = survey_loader(f'{main_path}{survey_group}.csv')
+validated = survey_loader(f'../../{survey_group}.csv')
 
 only_true_responses = validated.iloc[1:]
-question_answer_scheme = get_question_answer_scheme(validated)
 
-def group_columns(q_and_a_scheme):
-    main_groups = question_answer_scheme.keys().values
-    specific_groups = question_answer_scheme.iloc[0].values
-    return main_groups, specific_groups
+if survey_group == 'VS':
+    columns = scheme.staff
+elif survey_group == 'VC1':
+    columns = scheme.community1
+elif survey_group == 'VC2':
+    columns = scheme.community2
+    
+print(columns)
 
-general_q = np.arange(0,9)
+interesting_columns = columns[team_committee]
 
-main_groups, specific_groups = group_columns(question_answer_scheme)
+selected = only_true_responses.iloc[:,interesting_columns]
+print(selected)
 
-def get_q_indices(main_g):
-    q_start_stop = []
-    for k in range(len(main_g)):
-        if 'Unnamed' not in main_g[k]:
-            starts = k
-            for j in range(k,len(main_g)-1):
-                if 'Unnamed' not in main_g[j+1]:
-                    ends = j
-                    break
-            if k <= j:
-                q_start_stop.append([k,j])
-    return q_start_stop
-
-q_start_stop = get_q_indices(main_groups)
-
-def get_options(list_of_qs):
-    multi_q = []
-    single_q = []
-    for i,el in enumerate(list_of_qs):
-        if el[0] != el[1]:
-            multi_q.append(i)
-        else:
-            single_q.append(i)
-    return multi_q, single_q
-
-def view_one_q(df, all_q, q):
-    return df.iloc[:,all_q[q][0]:all_q[q][1]+1]
-
-qs_with_multi_options, qs_with_single_option = get_options(q_start_stop)
-
-true_qs_with_single_option = qs_with_single_option[9:]
-
+selected.to_csv(f'{survey_group}_{team_committee}_selected.csv', sep=',', encoding='utf-8')
